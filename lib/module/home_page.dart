3500e650328086e3ca4ac36061c8d83dc6f39a2b
Aviral Sharma <imaviralsharma@gmail.com>
2024-09-17 03:26:01 +0530

    feat: Appbar

--- lib/module/home_page.dart
+++ lib/module/home_page.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  String formatDate(DateTime date) {
+    final DateFormat formatter = DateFormat('EEEE d MMMM y');
+    return formatter.format(date);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Appbar
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         systemOverlayStyle: SystemUiOverlayStyle.dark,
         elevation: 0,
-        title: Column(
-          children: [],
+        title: Center(
+          child: Column(
+            children: [
+              Text(
+                "Today",
+                style: GoogleFonts.ubuntu(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                formatDate(DateTime.now()),
+                style: GoogleFonts.ubuntu(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 18,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
