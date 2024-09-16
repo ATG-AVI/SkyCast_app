2833fbcf2acd2ce156130dbc732e0b6d14b91392
Aviral Sharma <imaviralsharma@gmail.com>
2024-09-17 03:42:16 +0530

    feat: Add card with Lottie animation and formatted text on HomePage

--- lib/module/home_page.dart
+++ lib/module/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -40,6 +41,55 @@ class HomePage extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+
+      body: SizedBox.expand(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * .5,
+              child: Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Lottie.asset(
+                        'assets/loc.json',
+                        height: 90,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          text: "text",
+                          style: GoogleFonts.ubuntu(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "subText",
+                              style: GoogleFonts.ubuntu(
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
